5bcf848cd451dcc5466727c53f1259a8e0afc719
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-28 12:39:27 +0530

    * Updated Bubble sort .

--- sorting/bubble/bubble.js
+++ sorting/bubble/bubble.js
@@ -1,15 +1,13 @@
 function bubble(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; J++) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
     }
   }
   return arr
 }
-let arra = [1, 2, 3, 4, 5]
+let arra = [6, 1, 2, 3, 4, 5]
 const sort = bubble(arra)
 console.log(sort)
