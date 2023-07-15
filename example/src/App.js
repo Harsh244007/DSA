3fe80167e5d4b2ef157cdbc9307352259f124fad
Harsh Patel <Patelharsh241999@gmail.com>
2023-07-15 10:01:06 +0530

    * More questions .

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer,createContext, useContext  } from "react";
+import React, { useReducer, createContext, useContext } from "react";
 
 // Reducer function
 const reducer = (state, action) => {
@@ -8,13 +8,12 @@ const reducer = (state, action) => {
       console.log(state, action, "after");
       return { count: state.count + 1 };
     case "DECREMENT":
-      if(state.count>0){
-      console.log(state, action, "after");
-      return { count: state.count - 1 };}
-      else{
-        
-        alert('decerement value is false')
-      return {count: 0} 
+      if (state.count > 0) {
+        console.log(state, action, "after");
+        return { count: state.count - 1 };
+      } else {
+        alert("decerement value is false");
+        return { count: 0 };
       }
     default:
       console.log(state, action, "after");
@@ -22,8 +21,8 @@ const reducer = (state, action) => {
   }
 };
 
-const App1 = React.memo(({children}) => {
-  console.log("renderd app1")
+const App1 = React.memo(({ children }) => {
+  console.log("renderd app1");
   const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   const handleIncrement = () => {
@@ -44,12 +43,11 @@ const App1 = React.memo(({children}) => {
   );
 });
 
-
-const App2 = React.memo(({children,ThemeContext}) => {
+const App2 = React.memo(({ children, ThemeContext }) => {
   // Initializing state using useReducer
-  console.log("renderd app2")
+  console.log("renderd app2");
   const [state, dispatch] = useReducer(reducer, { count: 0 });
-  const {theme,toggleTheme} = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   const handleIncrement = () => {
     dispatch("INCREMENT");
   };
@@ -65,44 +63,54 @@ const App2 = React.memo(({children,ThemeContext}) => {
       <button onClick={handleDecrement}>Decrement</button>
       <p>Current theme: {theme}</p>
       <button onClick={toggleTheme}>Toggle Theme</button>
-  {children}
+      {children}
     </div>
   );
 });
 const App = () => {
   // Initializing state using useReducer
-  console.log("renderd mainapp")
+  console.log("renderd mainapp");
   const ThemeContext = createContext();
 
-  
-const themeReducer = (state, action) => {
-  switch (action) {
-    case 'TOGGLE_THEME':
-      return state === 'light' ? 'dark' : 'light';
-    default:
-      return state;
-  }
-};
+  const themeReducer = (state, action) => {
+    switch (action) {
+      case "TOGGLE_THEME":
+        return state === "light" ? "dark" : "light";
+      default:
+        return state;
+    }
+  };
   const ThemeProvider = ({ children }) => {
-    const [theme, dispatch] = useReducer(themeReducer, 'light');
+    const [theme, dispatch] = useReducer(themeReducer, "light");
     const toggleTheme = () => {
-      dispatch('TOGGLE_THEME');
+      dispatch("TOGGLE_THEME");
     };
     return (
-      <ThemeContext.Provider value={{theme,toggleTheme}}>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
         {children}
       </ThemeContext.Provider>
     );
   };
+  function MyComponent() {
+    return <div>jbhsadd</div>;
+  }
   return (
     <ThemeProvider>
-    <div>
-      <App1 key="app1" ThemeContext={ThemeContext}>
-      {()=><App2 key="app2" ThemeContext={ThemeContext}></App2>}
-      </App1>
-    </div>
+      <>
+      <div className="jbhax"></div>
+      <div className="jbhax2"></div>
+      </>
+      <div className="subroot">
+        {
+          console.log(<MyComponent />)
+          // console.log(MyComponent())
+        }
+        <App1 key="app1" ThemeContext={ThemeContext}>
+          {() => <App2 key="app2" ThemeContext={ThemeContext}></App2>}
+        </App1>
+      </div>
     </ThemeProvider>
   );
 };
 
-export default (App);
+export default App;
